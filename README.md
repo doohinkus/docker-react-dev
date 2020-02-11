8888ca2fd121e240e7413eaca42291cfee4e8fb1
doohinkus <doohinkus@gmail.com>
2020-02-10 18:30:51 -0800

    create a docker react poject

---
+++ README.md
@@ -0,0 +1,9 @@
+Docker container with react js.
+
+## To run
+
+```
+docker-compose up
+```
+
+Docker runs app in dev mode. Changes to files are automatically updated--see volume sharing in docker-compose.yml.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>YES!!! It's working!!! REALLY. I am not kidding!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
